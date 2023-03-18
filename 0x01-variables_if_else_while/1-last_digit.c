3b3207e4a3d28c1d2a49b66f0b308945fb8697c0
alpine57 <kamutileeon@gmail.com>
2023-03-18 02:47:12 -0700

    alphABET

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * Main - Entry point
  * Description: 'get last digit of the number'
  * Return: always 0
  */
