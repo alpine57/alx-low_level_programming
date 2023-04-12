11f1137760b48594a9a36aca2c30f76f92e51759
alpine57 <kamutileeon@gmail.com>
2023-04-12 13:28:42 -0700

    If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+*str_concat: Concatenates two strings
+*Return: s1 and s2
+**/
+char *str_concat(char *s1, char *s2)
+
+{
+	char *conct;
+	int x ,y;
+	
