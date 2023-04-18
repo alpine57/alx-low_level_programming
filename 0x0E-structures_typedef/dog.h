6e15d900126209c3732ff384fb3005f6af50847e
alpine57 <kamutileeon@gmail.com>
2023-04-18 07:47:00 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struc dog - dogs info
+*@name:name of the dog
+*@age:age of teh dog
+*
+*description:data base
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	char dog_t;
+};
+
+
+
+/**
+*dog_t - typedef for strut
+*/
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char*_strcpy(char*dest,char*src);
+
+
+#endif
