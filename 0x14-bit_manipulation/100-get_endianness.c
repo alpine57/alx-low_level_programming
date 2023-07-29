d6779ca8abf948f219ea52c02f3913e4e649e2f3
alpine57 <kamutileeon@gmail.com>
2023-07-29 01:32:12 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*get_endianness - checks if  machine is big
+*return :0 msuccess
+*/
+int get_endianness(void)
+{ 
+	unsigned int x = 1;
+	char *p = (char *)&x;
+
+
+	return(*p);
+}
