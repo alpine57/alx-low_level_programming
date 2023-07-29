14e2934b2449b1b66e581a4e0b7f55133bba77aa
alpine57 <kamutileeon@gmail.com>
2023-07-29 02:00:39 -0700

    bits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * set_bit - sets bit to a number.
- * @n: pointer to  number to be  changed.
+ *index: no to search for
+ *@n: pointer to  number to be  changed.
  * Return: 1 for success.
  */
 int set_bit(unsigned long int *n, unsigned int index)
