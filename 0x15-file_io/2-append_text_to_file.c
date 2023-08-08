a1ab69a5e1abd300b2e800a4e62ab7ef7fd6929a
alpine57 <kamutileeon@gmail.com>
2023-08-08 03:02:58 -0700

    file descriptors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,10 +10,10 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 
-        int o, w, len = 0;
+	int o, w, len = 0;
 
-        if (filename == NULL)
-       return (-1);
+	if (filename == NULL)
+	return (-1);
 
 	if (text_content != NULL)
 	{
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
-
+	
 	if (o == -1 || w == -1)
 
 	return (-1);
