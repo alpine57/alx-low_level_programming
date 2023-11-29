eb3992a9c4a8b5acfb8474b3f80016b7ff65549c
alpine57 <kamutileeongmail.com>
2023-11-29 00:10:43 -0800

    no of elements in list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,23 +2,21 @@
 
 /**
  * print_listint - prints all the elements of the linked list
- * @h: a node (memeber of the linked list)
- * Return: Number of element in the linked list
+ * @h: a node (member of the linked list)
+ * Return: Number of elements in the linked list
  */
 
 size_t print_listint(const listint_t *h)
 {
+    size_t c = 0;
 
-	int i = 0;
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        c++;
+    }
 
-	if (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-
-	return (i);
-
-
+    return c;
 }
+
